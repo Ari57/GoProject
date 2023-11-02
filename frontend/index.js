6bbd0ee93f71a426d9c1ccdd39fbcb17d6d9cbe5
Nathan Devey-Skeete <njds1676@gmail.com>
2023-11-02 13:46:07 +0000

    Updated frontend

--- frontend/index.js
+++ frontend/index.js
@@ -1,8 +1,39 @@
-let form = document.forms["name-form"]
-form.addEventListener("submit", getData)
+function UpdateFormAction() {
+    // Get the input element by its id
+    var nameInput = document.getElementById("name");
+  
+    // Get the value entered by the user
+    var nameValue = nameInput.value;
+  
+    // Update the form action with the value from the input
+    var form = document.forms["name-form"];
+    form.action = "https://goformtrigger.azurewebsites.net/api/FormTrigger?name=" + nameValue;
+  
+    // Submit the form
+    form.submit();
+  }
 
 
-function getData() {
-var nameValue = document.getElementById("fname").value
-console.log(nameValue)
+/*
+var nameContainer = document.querySelector(".current-name")
+
+async function GetCounter() {
+        let response = await fetch("https://cors-anywhere.herokuapp.com/https://countertrigger.azurewebsites.net/api/http_trigger")
+        let counter = await response.json()
+        return counter;
+    }
+
+nameContainer.innerHTML = "Ye"
+
+GetCounter().then(counter => nameContainer.innerHTML = "Ye")
+    
+const check = (e) => {
+    const form = new FormData(e.target)
+    const name = form.get("fname")
+    console.log(name)
+    console.log("----------------------------------")
+
+    return fetch("https://goformtrigger.azurewebsites.net/api/FormTrigger?name= " + name)
+        .then(data => nameContainer.innerHTML = "Ye")
 }
+*/
